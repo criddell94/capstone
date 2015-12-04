8665ef289bae31badffc0be3aee07eb7c9e0390d
criddell <riddell.connor.m@gmail.com>
2015-12-03 19:18:51 -0600

    highlights changed word blue to know it's done something.

--- checkforcorrection.py
+++ checkforcorrection.py
@@ -7,7 +7,7 @@
 correction_dict = {"function":"FUNCTION", "test":"TEST"}
 
 # helper function
-def make_correction(correction_candid) :
+def make_correction(correction_candid):
     if not correction_candid or len(correction_candid) == 0 :
         return None
     
@@ -15,8 +15,8 @@ def make_correction(correction_candid) :
     correction = ""
     
     # loop over keys in dictionary and break when correction is found
-    for key in correction_dict :
-        if correction_candid == key :
+    for key in correction_dict:
+        if correction_candid == key:
             correction = correction_dict.get(key)
             break
 
@@ -30,30 +30,32 @@ def check_for_correction(dummy_arg):
     doc = XSCRIPTCONTEXT.getDocument()
     selection_supplier = doc.getCurrentController()
     indexAccess = selection_supplier.getSelection()
-    count = indexAccess.getCount()
+    count = indexAccess.getCount() # don't really need this right now. Only loop once..
 
+    # get cursor to write corrected word highlighted in blue
     text = doc.Text
-    cursor = text.createTextCursor()
+    vis_cursor = doc.getCurrentController().getViewCursor()
+    nvis_cursor= vis_cursor.getText().createTextCursorByRange(vis_cursor)
         
-    # loop over highlighted section
-    for i in range(count) :
+    # Don't really need this right now, maybe later. (loop that is)
+    for i in range(count):
         # set str and txt for use
         highlighted_txt = indexAccess.getByIndex(i)
         highlighted_str = highlighted_txt.getString()
         
         # check if nothing is highlighted & return nothing if nothing is 
-        if len(highlighted_str) == 0 :
+        if len(highlighted_str) == 0:
             return None
 
         # call make_correction helper function to correct highlighted word
-        else :
+        else:
             correction = make_correction(highlighted_str)
             
             # make sure new correction word is present
             if correction:
-                #highlighted_txt.setString(correction)
-                #selection_supplier.select(highlighted_txt)
-                cursor.setPropertyValue( "CharColor", 255 )
-                text.insertString(cursor, correction, 0)
+                highlighted_txt.setString("") # replace old word with nothing
+                selection_supplier.select(highlighted_txt)
+                nvis_cursor.setPropertyValue("CharBackColor", 13421823) # set background to blue
+                text.insertString(nvis_cursor, correction, 0) # write correction
 
 g_exportedScripts = check_for_correction,
